32d05a73b70ce37575f714ade439270c0d049d47
Meiharish <meiharishgokul.s2024cce@sece.ac.in>
2025-07-08 11:59:40 +0530

    Add files via upload

---
+++ trainijava.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("hi");
+        System.out.println("the class is silent");
+        System.out.println("helloo ironheart");
+    }
+}
