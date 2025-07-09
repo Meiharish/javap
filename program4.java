87304f2b9dc0d306f96b8ff5ca68008e4589f0a8
Meiharish <meiharishgokul.s2024cce@sece.ac.in>
2025-07-09 09:50:14 +0530

    Add files via upload

---
+++ program4.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        String name;
+        String country;
+        String city;
+        String street;
+        name='Harish';
+        country='India';
+        city='Tirupattur';
+        street='BKM';
+        System.out.println("Name:"+name);
+        System.out.println("country:"+country);
+        System.out.println("city:"+city);
+        System.out.println("street:"+street);
+    }
+}
